1003b7fa26f4f16b33225297ca190a92e606d208
Karaulov <unrealkaraulov@bk.ru>
2022-12-18 18:16:41 +0300

    Update Gui.cpp

--- src/editor/Gui.cpp
+++ src/editor/Gui.cpp
@@ -514,7 +514,7 @@ void ExportModel(Bsp* map, int id, int ExportType)
 	{
 		tmpMap->update_ent_lump();
 		tmpMap->update_lump_pointers();
-		tmpMap->write(GetWorkDir() + "_" + map->bsp_name + "_model" + std::to_string(id) + ".bsp");
+		tmpMap->write(GetWorkDir() + map->bsp_name + "_model" + std::to_string(id) + ".bsp");
 	}
 }
 
